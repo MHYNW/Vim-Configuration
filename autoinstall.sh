bff7be7cb6bfaaabd5858479db4c539ebcedd817
MHYNW <phdcarrot@gmail.com>
2023-01-06 13:33:53 +0900

    ymcd error handled in scripts

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+CWD=`pwd`
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp .vimrc ~/
+
+cd ~/.vim/plugged/youcompleteme
+git submodule update --init --recursive
+./install.py
+
+cd $CWD
 
