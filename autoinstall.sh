eaa7aade762b12192c662b333dfeca2307773b8d
MHYNW <phdcarrot@gmail.com>
2023-01-08 23:48:43 +0900

    230108_update

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,10 +6,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp .vimrc ~/
-
-cd ~/.vim/plugged/youcompleteme
-git submodule update --init --recursive
-./install.py
-
-cd $CWD
-
